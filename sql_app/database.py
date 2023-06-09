3ced720539212755dcad10ca3d1b659c416172b6
Charles-Rocke <89711262+Charles-Rocke@users.noreply.github.com>
2023-06-09 18:13:41 +0000

    initial commit for master branch

--- sql_app/database.py
+++ sql_app/database.py
@@ -11,14 +11,21 @@ from sqlalchemy.orm import sessionmaker
 # )
 
 # For postgres Testing DB below
-SQLALCHEMY_DATABASE_URL = "postgresql://blocsquad_api_test_db:eZBhN2vVO9lxtjf6LsFav5EzIt4PmvTh@dpg-ci0ame3hp8ue00dkdjn0-a.ohio-postgres.render.com/bloc_api_test_db"
+# SQLALCHEMY_DATABASE_URL = "postgresql://blocsquad_api_test_db:eZBhN2vVO9lxtjf6LsFav5EzIt4PmvTh@dpg-ci0ame3hp8ue00dkdjn0-a.ohio-postgres.render.com/bloc_api_test_db"
+
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL,
+#     pool_pre_ping=True,
+# )
+
+# For postgres Production DB will go below
+SQLALCHEMY_DATABASE_URL = "postgresql://blocsquad_api_prod_db:1ilC1uCGyv2GSKFib3nA4AjszT8C0MBU@dpg-ci1mg6g2qv2fv4rvj8qg-a.ohio-postgres.render.com/bloc_api_prod_db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     pool_pre_ping=True,
 )
 
-# For postgres Production DB will go below
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
